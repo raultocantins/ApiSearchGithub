9edd47144a2642a29c3d0943cf8c1501373416a4
raultocantins <alexbraul.ar@gmail.com>
2021-03-16 15:09:44 -0300

    Api for search all user repositories and others repositories in Github

---
+++ app.js
@@ -0,0 +1,29 @@
+const githubApi = require("./githubApi");
+const express = require("express");
+require("dotenv").config();
+const app = express();
+app.use(express.urlencoded());
+app.use(express.json());
+
+//buscar todos os repositorios do usuario
+app.get("/repositories/:user", async (req, res) => {
+  const user = req.params.user;
+  const repos = await githubApi.repositories(`${user}`);
+  if (repos) {
+    res.json(repos);
+  } else {
+    res.send("Nenhum Repositorio !!!");
+  }
+});
+//Procurar algum repositorio no github.
+app.post("/search/code", async (req, res) => {
+  const search = req.body.search;
+ 
+  
+const result = await githubApi.search(`${search}`);
+res.json(result)
+
+});
+app.listen(process.env.PORT_APP, () => {
+  console.log("Server in Progress...");
+});
---
+++ githubApi.js
@@ -0,0 +1,59 @@
+const { Octokit } = require("@octokit/core");
+const octokit = new Octokit();
+
+//buscar repositorios
+ async function repositories(username) {
+  var result = await octokit
+    .request("GET /users/{username}/repos", {
+      username: username,
+    })
+    .then((res) =>
+      res.data.map((e) => {
+        return {
+          name: e.name,
+          full_name: e.full_name,
+          description: e.description,
+        };
+      })
+    ).catch(err=>console.log(err))
+  return result
+}
+
+ async function issues() {
+  var issues = await octokit
+    .request("GET /issues")
+    .then((res) => res.data)
+    .catch((err) => err);
+  return issues
+}
+
+ async function pulls() {
+  var pulls = await octokit
+    .request("GET /repos/{owner}/{repo}/pulls/comments", {
+      owner: "raultocantins",
+      repo: "toDoList",
+    })
+    .then((res) => res)
+    .catch((err) => err);
+  return pulls
+}
+
+
+ async function search(value) {
+  var result = await octokit.request("GET /search/repositories", {
+    q: `${value}`
+    
+  });
+  var data={
+    total_count:result.data.total_count,
+    items:result.data.items
+  }
+
+  return data;
+}
+
+
+module.exports={
+  repositories,issues,search,pulls
+}
+
